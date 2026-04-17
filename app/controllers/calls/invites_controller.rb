7a31c5844e3e7e5fee10158778f8e9469301a71b
Ritwick Malhotra <malhotraritwick2011@gmail.com>
2026-04-17 12:37:53 +0200

    pwa keyboard fixes, redirect bug fix

--- app/controllers/calls/invites_controller.rb
+++ app/controllers/calls/invites_controller.rb
@@ -4,15 +4,37 @@ module Calls
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
     def show
+      restore_authentication unless Current.user
       token = Calls::Invite.normalize_token(params[:token])
       invite = Calls::Invite.active.find_by!(token: token)
       destination_url = Calls::Invite.trusted_destination_url(invite.destination_url)
       return render_not_found unless destination_url
 
-      head :found, location: destination_url
+      head :found, location: redirect_destination(invite: invite, raw_url: destination_url)
     end
 
     private
+      def redirect_destination(invite:, raw_url:)
+        uri = URI.parse(raw_url)
+        room_slug = uri.path.to_s.delete_prefix("/").split("/").first
+        base_pairs = URI.decode_www_form(uri.query.to_s).reject { |key, _| key == "jwt" }
+        jwt = member_jitsi_jwt_for(invite.room, room_slug)
+        base_pairs << [ "jwt", jwt ] if jwt.present?
+
+        uri.query = base_pairs.any? ? URI.encode_www_form(base_pairs) : nil
+        uri.to_s
+      rescue URI::InvalidURIError
+        raw_url
+      end
+
+      def member_jitsi_jwt_for(room, room_slug)
+        return nil unless Current.user
+        return nil if room_slug.blank?
+        return nil unless room.memberships.exists?(user_id: Current.user.id)
+
+        Calls::JitsiTokenBuilder.call(room_slug: room_slug, creator: Current.user)
+      end
+
       def render_not_found
         head :not_found
       end
--- test/controllers/calls/invites_controller_test.rb
+++ test/controllers/calls/invites_controller_test.rb
@@ -3,6 +3,17 @@ require "test_helper"
 class Calls::InvitesControllerTest < ActionDispatch::IntegrationTest
   setup do
     host! "once.campfire.test"
+    @previous_values = {
+      "JITSI_JWT_APP_ID" => ENV["JITSI_JWT_APP_ID"],
+      "JITSI_JWT_APP_SECRET" => ENV["JITSI_JWT_APP_SECRET"],
+      "JITSI_JWT_AUDIENCE" => ENV["JITSI_JWT_AUDIENCE"],
+      "JITSI_JWT_SUBJECT" => ENV["JITSI_JWT_SUBJECT"],
+      "JITSI_JWT_TTL_SECONDS" => ENV["JITSI_JWT_TTL_SECONDS"]
+    }
+  end
+
+  teardown do
+    @previous_values.each { |key, value| ENV[key] = value }
   end
 
   test "redirects to destination for active invite token" do
@@ -15,7 +26,7 @@ class Calls::InvitesControllerTest < ActionDispatch::IntegrationTest
 
     get call_invite_url(token: invite.token)
 
-    assert_redirected_to invite.destination_url
+    assert_redirected_to "https://meet.daiwick.com/room-123"
   end
 
   test "redirects via short /c route" do
@@ -28,7 +39,7 @@ class Calls::InvitesControllerTest < ActionDispatch::IntegrationTest
 
     get short_call_invite_url(token: invite.token)
 
-    assert_redirected_to invite.destination_url
+    assert_redirected_to "https://meet.daiwick.com/room-123"
   end
 
   test "returns not found for unknown token" do
@@ -60,7 +71,20 @@ class Calls::InvitesControllerTest < ActionDispatch::IntegrationTest
 
     get call_invite_url(token: "#{invite.token}\u00A0")
 
-    assert_redirected_to invite.destination_url
+    assert_redirected_to "https://meet.daiwick.com/room-123"
+  end
+
+  test "removes only jwt query param from redirect destination" do
+    invite = Calls::Invite.create!(
+      room: rooms(:designers),
+      creator: users(:david),
+      destination_url: "https://meet.daiwick.com/room-123?foo=bar&jwt=abc&baz=1#config.prejoinConfig.enabled=false",
+      expires_at: 30.minutes.from_now
+    )
+
+    get call_invite_url(token: invite.token)
+
+    assert_redirected_to "https://meet.daiwick.com/room-123?foo=bar&baz=1#config.prejoinConfig.enabled=false"
   end
 
   test "returns not found for untrusted destination host" do
@@ -76,5 +100,59 @@ class Calls::InvitesControllerTest < ActionDispatch::IntegrationTest
     get call_invite_url(token: invite.token)
 
     assert_response :not_found
+  end
+
+  test "adds member-specific jwt for authenticated room member" do
+    ENV["JITSI_JWT_APP_ID"] = "campfire"
+    ENV["JITSI_JWT_APP_SECRET"] = "secret"
+    ENV["JITSI_JWT_AUDIENCE"] = "jitsi"
+    ENV["JITSI_JWT_SUBJECT"] = "meet.daiwick.com"
+    ENV["JITSI_JWT_TTL_SECONDS"] = "3600"
+    sign_in(:david)
+
+    invite = Calls::Invite.create!(
+      room: rooms(:designers),
+      creator: users(:david),
+      destination_url: "https://meet.daiwick.com/room-123?jwt=creator-token",
+      expires_at: 30.minutes.from_now
+    )
+
+    get call_invite_url(token: invite.token)
+
+    redirected = URI.parse(response.redirect_url)
+    jwt = CGI.parse(redirected.query.to_s)["jwt"].first
+    payload, = JWT.decode(jwt, "secret", true, algorithm: "HS256")
+
+    assert_equal "/room-123", redirected.path
+    assert_equal users(:david).id.to_s, payload.dig("context", "user", "id")
+  end
+
+  test "does not add jwt for authenticated user outside room" do
+    ENV["JITSI_JWT_APP_ID"] = "campfire"
+    ENV["JITSI_JWT_APP_SECRET"] = "secret"
+    outsider = User.create!(
+      name: "Outside Member",
+      email_address: "outside-member@example.com",
+      password: "secret123456",
+      password_confirmation: "secret123456"
+    )
+    AccountMembership.create!(user: outsider, account: rooms(:designers).account)
+    sign_in(outsider)
+
+    invite = Calls::Invite.create!(
+      room: rooms(:designers),
+      creator: users(:david),
+      destination_url: "https://meet.daiwick.com/room-123?foo=bar&jwt=creator-token",
+      expires_at: 30.minutes.from_now
+    )
+
+    get call_invite_url(token: invite.token)
+
+    redirected = URI.parse(response.redirect_url)
+    query = CGI.parse(redirected.query.to_s)
+
+    assert_equal "/room-123", redirected.path
+    assert_equal [ "bar" ], query["foo"]
+    assert_not query.key?("jwt")
   end
 end
