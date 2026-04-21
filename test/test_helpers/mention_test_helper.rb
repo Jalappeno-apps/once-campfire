ba174fe7769baca51714c4044119c591b838c4b8
Ritwick Malhotra <malhotraritwick2011@gmail.com>
2026-04-21 17:25:46 +0200

    updated specs

--- test/test_helpers/mention_test_helper.rb
+++ test/test_helpers/mention_test_helper.rb
@@ -2,6 +2,7 @@ module MentionTestHelper
   def mention_attachment_for(name)
     user = users(name)
     attachment_body = ApplicationController.render partial: "users/mention", locals: { user: user }
-    "<action-text-attachment sgid=\"#{user.attachable_sgid}\" content-type=\"application/vnd.campfire.mention\" content=\"#{attachment_body.gsub('"', '&quot;')}\"></action-text-attachment>"
+    safe_content = attachment_body.gsub('"', '&quot;').gsub("%", "%25")
+    "<action-text-attachment sgid=\"#{user.attachable_sgid}\" content-type=\"application/vnd.campfire.mention\" content=\"#{safe_content}\"></action-text-attachment>"
   end
 end
